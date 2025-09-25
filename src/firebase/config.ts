0a316ae142e315860e5d03b8968f1283c2fc45ca
Esra Akgün <esrakgun.409@gmail.com>
2025-09-25 08:44:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "vetai-mfu5p",
+  "appId": "1:493607074502:web:488f043e1139e0f28ab5c7",
+  "apiKey": "AIzaSyDo2o3gnx0OOLemnSWOeEDp7kMP8bWabo0",
+  "authDomain": "vetai-mfu5p.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "493607074502"
+};
